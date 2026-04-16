9d6ef57b0c3250cf62bb3181350f95beb68cdbbf
Rahul3658 <rahuldc3658@gmail.com>
2026-04-16 11:27:53 +0530

    Update 60-vault.sh

--- .docker/entrypoint/60-vault.sh
+++ .docker/entrypoint/60-vault.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-export VAULT_ADDR="http://vault:8200"
-export VAULT_TOKEN="root"
-
 vault kv get -format=json secret/laravel \
 | jq -r '.data.data | to_entries[] | "\(.key)=\(.value)"' \
 > /var/www/html/.env
 
-until nc -z mysql 3306; do
+echo "Waiting for MySQL..."
+
+until nc -z laravel-mysql 3306; do
   sleep 2
 done
+
+echo "Startup complete"
